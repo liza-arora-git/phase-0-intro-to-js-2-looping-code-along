1688df1e9e5256c5aff08fc27284b31c45151f20
Liza Arora <liza.arora@outlook.com>
2022-05-31 02:44:19 +1000

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solutions in this file
+// for (let age=30; age<40; age ++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+    
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return(name);
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+//Function using arrow
+writeCards = (name, event) => {
+    for (let i = 0; i < name.length; i++) {
+        console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return(name);
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+
+
+let countup = 0;
+while(countup<10) {
+    console.log(countup++);
+}
+
+function countDown() {
+    let countdown = 10;
+    while(countdown>=0) {
+        console.log(countdown--);
+}
+}
+countDown(10);
